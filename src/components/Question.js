afd35ffcdd4e48ef908c98ae47580ed15d6f4c57
Dylan <dylanlancekemboi@gmail.com>
2023-11-14 13:30:23 +0300

    Completed Assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      // decreasing time by 1 second
+      setTimeRemaining((prevTime) => Math.max(0, prevTime - 1));
+    }, 1000);
+
+    // Cleanup function for useEffect
+    return () => clearTimeout(timerId);
+  }, [timeRemaining]); // Dependency array to ensure useEffect runs when timeRemaining changes
+
+  useEffect(() => {
+    // to check if time runs out
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      // call the onAnswered callback with a value of false
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
